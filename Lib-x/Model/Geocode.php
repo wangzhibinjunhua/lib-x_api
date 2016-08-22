4c834d203e37ac97b00650e8cc3b4c45ca498355
wzb <wangzhibin_x@qq.com>
2016-08-22 10:25:44 +0800

    modify geocode pai 网络请求方式

--- Lib-x/Model/Geocode.php
+++ Lib-x/Model/Geocode.php
@@ -6,12 +6,8 @@ class Model_Geocode extends PhalApi_Model_NotORM
 		//高德逆地理编码
 		$url='http://restapi.amap.com/v3/geocode/regeo?location='.$location.'&key=7fea101018f20db09c162c6f2ff8b4bd';
 		//var_export($url);
-		$client = Common_HttpClient::create()
-		->withHost($url);
-		$rs = $client->reset()
-		->withTimeout(3000)
-		->request();
-	
+		$curl=new PhalApi_CUrl();
+		$rs=$curl->get($url);
 		return $rs;
 	
 	}
