6b88193925639f20254107ac2156b1f71732ea30
wzb <wangzhibin_x@qq.com>
2016-08-31 15:00:54 +0800

    update watch get_lastest_location api

--- Lib-x/Model/Watch.php
+++ Lib-x/Model/Watch.php
@@ -4,7 +4,7 @@ class Model_Watch extends PhalApi_Model_NotORM
 	
 	public function get_lastest_location($imei)
 	{
-		$sql='select watch_time,location_lon,location_lat,location_type,battery from watch_info where imei = '.$imei.' order by id desc limit 0 ,1';
+		$sql='select watch_time,location_lon,location_lat,location_type,location_content,battery from watch_info where imei = '.$imei.' order by id desc limit 0 ,1';
 		return $this->getORM('watch')
 		->queryAll($sql);
 	}
