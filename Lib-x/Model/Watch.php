535b091ed102e609a05f51a3497e6e98f71a37a8
wzb <wangzhibin_x@qq.com>
2016-09-20 15:56:02 +0800

    update watch api

--- Lib-x/Model/Watch.php
+++ Lib-x/Model/Watch.php
@@ -1,22 +1,23 @@
 <?php
 class Model_Watch extends PhalApi_Model_NotORM
 {
-	
+
 	public function get_lastest_location($imei)
 	{
-		$sql='select watch_time,location_lon,location_lat,location_type,location_content,battery from watch_info where imei = '.$imei.' order by id desc limit 0 ,1';
+		$sql='select watch_time,location_lon,location_lat,location_type,location_content,battery from watch_info where imei = :imei order by id desc limit 0 ,1';
+		$params=array(':imei'=> $imei);
 		return $this->getORM('watch')
-		->queryAll($sql);
+		->queryAll($sql,$params);
 	}
-	
+
 	public function get_day_location($imei,$date)
 	{
-		$date=" '$date' ";
-		$sql='select location_lon,location_lat from watch_info where imei = '.$imei.' and date(watch_time)='.$date.' ';
+		$sql='select location_lon,location_lat from watch_info where imei = :imei and date(watch_time) = :datetime';
+		$params=array(':imei'=> $imei,':datetime'=> $date);
 		return $this->getORM('watch')
-		->queryAll($sql);
+		->queryAll($sql,$params);
 	}
-	
+
 	protected function getTableName($id)
 	{
 		return 'info';
