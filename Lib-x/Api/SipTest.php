6ce74faeb734ada6f2c8867507cb64a455b947fd
wzb <wangzhibin_x@qq.com>
2018-01-09 17:42:11 +0800

    siptest api

--- Lib-x/Api/SipTest.php
+++ Lib-x/Api/SipTest.php
@@ -1,55 +1,145 @@
 <?php
-DI()->response->setResType(1);
-class Api_SipTest extends PhalApi_Api
-{
-	
-	public function getRules()
-	{
-		return array(
-		
-				'login'=>array(
-						'username'=>array('name' =>'username' ,'require'=>true),
-						'password'=>array('name' =>'password' ,'require'=>true),
+DI ()->response->setResType ( 1 );
+class Api_SipTest extends PhalApi_Api {
+	public function getRules() {
+		return array (
+				
+				'login' => array (
+						'username' => array (
+								'name' => 'username',
+								'require' => true 
+						),
+						'password' => array (
+								'name' => 'password',
+								'require' => true 
+						) 
 				),
-				'register'=>array(
-						'mobile'=>array('name' =>'mobile' ,'min'=>11,'max'=>11,'require'=>true),
-						'vkey'=>array('name' =>'vkey' ,'min'=>6,'require'=>true),
-						'password'=>array('name' =>'password' ,'min'=>6,'require'=>true),
+				
+				'setNetWorkStatic' => array (
+						'ip' => array (
+								'name' => 'ip',
+								'require' => true
+						),
+						'mask' => array (
+								'name' => 'mask',
+								'require' => true
+						),
+						'gate' => array (
+								'name' => 'gate',
+								'require' => true
+						),
+						'dns1' => array (
+								'name' => 'dns1',
+								'require' => true
+						),
+						'dns2' => array (
+								'name' => 'dns2',
+								'require' => true
+						)
 				),
+				'register' => array (
+						'mobile' => array (
+								'name' => 'mobile',
+								'min' => 11,
+								'max' => 11,
+								'require' => true 
+						),
+						'vkey' => array (
+								'name' => 'vkey',
+								'min' => 6,
+								'require' => true 
+						),
+						'password' => array (
+								'name' => 'password',
+								'min' => 6,
+								'require' => true 
+						) 
+				) 
 		);
 	}
-	
-	public function login()
-	{
-
-		$rs=array('code'=> 0,'message'=>'Login Failed','info'=>'');
-		if($this->username=='admin' && $this->password == 'admin'){
-			$rs['code']=1;
-			$rs['message']="Login Succeed";
+	public function login() {
+		$rs = array (
+				'code' => 0,
+				'message' => 'Login Failed',
+				'info' => '' 
+		);
+		if ($this->username == 'admin' && $this->password == 'admin') {
+			$rs ['code'] = 1;
+			$rs ['message'] = "Login Succeed";
 		}
 		return $rs;
 	}
-	
-	
-	/**
-	* @author wzb<wangzhibin_x@foxmail.com>
-	* @date Oct 28, 2016 3:36:34 PM
-	* 注册新用户
-	* @param mobile
-	* @param vkey
-	* @param password(明文,服务端加密)
-	* @return string(json) ok/fail
-	*/
-	public function register()
-	{
-		$rs=array('code'=> 0,'message'=>'','info'=>'');
-		$model=new Model_Account();
-		$rs=$model->register($this->mobile,$this->vkey,$this->password);
+	public function getSysInfo() {
+		$rs = array (
+				'code' => 1,
+				'model' => 'HA05',
+				'hwv' => '1.0',
+				'swv' => 'HA05_v01_20180109',
+				'upt' => '28:37:22' 
+		);
 		return $rs;
 	}
 	
+	public function getNewStatus()
+	{
+		$rs = array (
+				'code' => 1,
+				'type' => 'DHCP',
+				'mac' => '00:a8:59:f5:d4:39',
+				'ip' => '192.168.0.122',
+				'mask' => '255.255.255.0',
+				'gate'=>'192.168.0.1'
+		);
+		
+		return $rs;
+	}
 	
-
-
-
+	public function setNetWorkStatic()
+	{
+		$rs = array (
+				'code' => 1,
+				'message' => 'set ok',
+				'info' => ''
+		);
+		return $rs;
+	}
+	
+	public function setNetWorkDhcp()
+	{
+		$rs = array (
+				'code' => 1,
+				'message' => 'set ok',
+				'info' => ''
+		);
+		return $rs;
+	}
+	
+	public function getSipInfo()
+	{
+		
+	}
+	
+	/**
+	 *
+	 * @author wzb<wangzhibin_x@foxmail.com>
+	 *         @date Oct 28, 2016 3:36:34 PM
+	 *         注册新用户
+	 * @param
+	 *        	mobile
+	 * @param
+	 *        	vkey
+	 * @param
+	 *        	password(明文,服务端加密)
+	 * @return string(json) ok/fail
+	 */
+	public function register() {
+		$rs = array (
+				'code' => 0,
+				'message' => '',
+				'info' => '' 
+		);
+		$model = new Model_Account ();
+		$rs = $model->register ( $this->mobile, $this->vkey, $this->password );
+		return $rs;
+	}
 }
